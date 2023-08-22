8cf6bb1b47eea25e99806abf564da13507370129
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-22 22:15:14 +0900

    [Account Screen] rendered title

--- lib/authentication/account_screen.dart
+++ lib/authentication/account_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/gaps.dart';
 import 'package:twitter_clone/constants/sizes.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -19,7 +20,7 @@ class AccountScreen extends StatelessWidget {
           color: Color(0xFF4E98E9),
           size: Sizes.size20 + 10,
         ),
-        leadingWidth: 90,
+        leadingWidth: 100,
         leading: TextButton(
           onPressed: () => onInitialTap(context),
           child: const Text(
@@ -32,6 +33,22 @@ class AccountScreen extends StatelessWidget {
         ),
         backgroundColor: Theme.of(context).secondaryHeaderColor,
         elevation: 0,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: Sizes.size40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v20,
+            Text(
+              "Create your account",
+              style: TextStyle(
+                fontSize: Sizes.size28 + 2,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
