0802c7c3b158e744b45812f8ed99f76fbb811089
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-23 16:06:31 +0900

    [Experience Screen] Experience screen start

--- lib/authentication/experience_screen.dart
+++ lib/authentication/experience_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/sizes.dart';
 
 class ExperienceScreen extends StatelessWidget {
   const ExperienceScreen({super.key});
 
+  void onInitialTap(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Theme.of(context).secondaryHeaderColor,
+      appBar: AppBar(
+        title: const FaIcon(
+          FontAwesomeIcons.twitter,
+          color: Color(0xFF4E98E9),
+          size: Sizes.size20 + 10,
+        ),
+        leadingWidth: 100,
+        leading: TextButton(
+          onPressed: () => onInitialTap(context),
+          child: const Text(
+            'Cancel',
+            style: TextStyle(
+              color: Color(0xFF101318),
+              fontSize: Sizes.size16 + 2,
+            ),
+          ),
+        ),
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        elevation: 0,
+      ),
+    );
   }
 }
