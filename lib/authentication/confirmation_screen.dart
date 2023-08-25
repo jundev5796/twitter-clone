b3e2c5aa0c63a398e35c078bc85f442d089d8d5b
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-25 20:35:44 +0900

    [Confirmation Screen] Implemented textfields to disable when clicking on anywhere in the scaffold

--- lib/authentication/confirmation_screen.dart
+++ lib/authentication/confirmation_screen.dart
@@ -15,6 +15,7 @@ class ConfirmationScreen extends StatefulWidget {
 class _ConfirmationScreenState extends State<ConfirmationScreen> {
   bool isButtonEnabled = false;
   bool showCheckIcon = false;
+  bool disableFields = false;
 
   List<TextEditingController> controllers =
       List.generate(6, (index) => TextEditingController());
@@ -35,175 +36,200 @@ class _ConfirmationScreenState extends State<ConfirmationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const FaIcon(
-          FontAwesomeIcons.twitter,
-          color: Color(0xFF4E98E9),
-          size: Sizes.size20 + 10,
-        ),
-        leadingWidth: 100,
-        leading: IconButton(
-          onPressed: () => onInitialTap(context),
-          icon: const Padding(
-            padding:
-                EdgeInsets.only(right: 40), // Adjust this value to your needs
-            child: FaIcon(
-              FontAwesomeIcons.arrowLeft,
-              color: Color(0xFF101318),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+        setState(() {
+          disableFields = true;
+        });
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const FaIcon(
+            FontAwesomeIcons.twitter,
+            color: Color(0xFF4E98E9),
+            size: Sizes.size20 + 10,
+          ),
+          leadingWidth: 100,
+          leading: IconButton(
+            onPressed: () => onInitialTap(context),
+            icon: const Padding(
+              padding:
+                  EdgeInsets.only(right: 40), // Adjust this value to your needs
+              child: FaIcon(
+                FontAwesomeIcons.arrowLeft,
+                color: Color(0xFF101318),
+              ),
             ),
           ),
+          backgroundColor: Theme.of(context).secondaryHeaderColor,
+          elevation: 0,
         ),
-        backgroundColor: Theme.of(context).secondaryHeaderColor,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size40,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v20,
-            const Text(
-              "We sent you a code",
-              style: TextStyle(
-                fontSize: Sizes.size28 + 2,
-                fontWeight: FontWeight.w800,
-              ),
-            ),
-            Gaps.v20,
-            Text(
-              "Enter it below to verify",
-              style: TextStyle(
-                color: Colors.grey.shade700,
-                fontSize: Sizes.size16 + 2,
-              ),
-            ),
-            Gaps.v2,
-            Text(
-              "${widget.email}.",
-              style: TextStyle(
-                color: Colors.grey.shade700,
-                fontSize: Sizes.size16 + 2,
-              ),
-            ),
-            Gaps.v60,
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  6,
-                  (index) => Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 5),
-                    width: 48,
-                    child: TextField(
-                      style: const TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.w800,
-                      ),
-                      controller: controllers[index],
-                      focusNode: focusNodes[index],
-                      keyboardType: TextInputType.number,
-                      textAlign: TextAlign.center,
-                      maxLength: 1,
-                      onChanged: (value) {
-                        if (value.isNotEmpty && index < 5) {
-                          focusNodes[index + 1].requestFocus();
-                        }
-                        checkIfAllFilled(); // Add this line
-                      },
-                      decoration: const InputDecoration(
-                        counterText: "", // To hide the counter text
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.blue),
-                        ),
-                      ),
-                    ),
-                  ),
-                ).toList(),
-              ),
-            ),
-            Gaps.v28,
-            if (showCheckIcon)
-              const Center(
-                child: FaIcon(
-                  FontAwesomeIcons.circleCheck,
-                  color: Colors.green,
-                  size: 30,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v20,
+              const Text(
+                "We sent you a code",
+                style: TextStyle(
+                  fontSize: Sizes.size28 + 2,
+                  fontWeight: FontWeight.w800,
                 ),
               ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
-        elevation: 0,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 18,
-                  horizontal: 40,
-                ),
-                child: Text(
-                  "Didn't receive email?",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                  ),
+              Gaps.v20,
+              Text(
+                "Enter it below to verify",
+                style: TextStyle(
+                  color: Colors.grey.shade700,
+                  fontSize: Sizes.size16 + 2,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
+              Gaps.v2,
+              Text(
+                "${widget.email}.",
+                style: TextStyle(
+                  color: Colors.grey.shade700,
+                  fontSize: Sizes.size16 + 2,
+                ),
               ),
-              child: AnimatedContainer(
-                duration: const Duration(
-                    milliseconds: 300), // animation duration for the container
-                curve: Curves.easeOut, // easing for the container
-                padding: const EdgeInsets.symmetric(
-                  vertical: 16,
-                  horizontal: 10,
-                ),
-                decoration: BoxDecoration(
-                  color: isButtonEnabled
-                      ? const Color(0xFF101318)
-                      : Colors.grey, // conditional color
-                  borderRadius: BorderRadius.circular(27),
-                  border: Border.all(
-                    color: Colors.grey.shade300,
-                    width: Sizes.size2,
-                  ),
-                ),
+              Gaps.v60,
+              Center(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    AnimatedDefaultTextStyle(
-                      style: TextStyle(
-                        color: isButtonEnabled
-                            ? Colors.white
-                            : const Color.fromARGB(255, 229, 226, 226),
-                        fontSize: Sizes.size16 + 3,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      duration: const Duration(
-                          milliseconds: 300), // animation duration for the text
-                      // easing for the text
-                      child: const Text('Next'),
-                    ),
-                  ],
+                  children: List.generate(
+                    6,
+                    (index) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        width: 48,
+                        child: GestureDetector(
+                          behavior: HitTestBehavior.translucent,
+                          onTap: () {
+                            if (disableFields) {
+                              setState(() {
+                                disableFields = false;
+                              });
+                              focusNodes[index]
+                                  .requestFocus(); // Manually requesting focus
+                            }
+                          },
+                          child: AbsorbPointer(
+                            absorbing:
+                                disableFields, // Using AbsorbPointer again to handle taps more efficiently
+                            child: TextField(
+                              style: const TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.w800,
+                              ),
+                              controller: controllers[index],
+                              focusNode: focusNodes[index],
+                              keyboardType: TextInputType.number,
+                              textAlign: TextAlign.center,
+                              maxLength: 1,
+                              onChanged: (value) {
+                                if (value.isNotEmpty && index < 5) {
+                                  focusNodes[index + 1].requestFocus();
+                                }
+                                checkIfAllFilled();
+                              },
+                              decoration: const InputDecoration(
+                                counterText: "", // To hide the counter text
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.grey),
+                                ),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.blue),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )),
+                  ).toList(),
                 ),
               ),
-            ),
-          ],
+              Gaps.v28,
+              if (showCheckIcon)
+                const Center(
+                  child: FaIcon(
+                    FontAwesomeIcons.circleCheck,
+                    color: Colors.green,
+                    size: 30,
+                  ),
+                ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          elevation: 0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 18,
+                    horizontal: 40,
+                  ),
+                  child: Text(
+                    "Didn't receive email?",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                ),
+                child: AnimatedContainer(
+                  duration: const Duration(
+                      milliseconds:
+                          300), // animation duration for the container
+                  curve: Curves.easeOut, // easing for the container
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: isButtonEnabled
+                        ? const Color(0xFF101318)
+                        : Colors.grey, // conditional color
+                    borderRadius: BorderRadius.circular(27),
+                    border: Border.all(
+                      color: Colors.grey.shade300,
+                      width: Sizes.size2,
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AnimatedDefaultTextStyle(
+                        style: TextStyle(
+                          color: isButtonEnabled
+                              ? Colors.white
+                              : const Color.fromARGB(255, 229, 226, 226),
+                          fontSize: Sizes.size16 + 3,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        duration: const Duration(
+                            milliseconds:
+                                300), // animation duration for the text
+                        // easing for the text
+                        child: const Text('Next'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
