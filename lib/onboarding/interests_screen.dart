7256ca7457305f42dd6bb38d9a4e1c45eed0735f
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-26 00:04:40 +0900

    [Interests Screen] Rendered Appbar

--- lib/onboarding/interests_screen.dart
+++ lib/onboarding/interests_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/sizes.dart';
 
 class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
@@ -10,6 +12,18 @@ class InterestsScreen extends StatefulWidget {
 class _InterestsScreenState extends State<InterestsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const FaIcon(
+          FontAwesomeIcons.twitter,
+          color: Color(0xFF4E98E9),
+          size: Sizes.size20 + 10,
+        ),
+        leadingWidth: 100,
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        elevation: 0,
+      ),
+    );
   }
 }
