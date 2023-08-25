c01764447d8bbe6eaea8b5a1e11a7c09ab96268e
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-25 20:44:24 +0900

    [Password Screen] Password Screen start

--- lib/authentication/password_screen.dart
+++ lib/authentication/password_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/sizes.dart';
 
 class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
@@ -8,8 +10,35 @@ class PasswordScreen extends StatefulWidget {
 }
 
 class _PasswordScreenState extends State<PasswordScreen> {
+  void onInitialTap(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const FaIcon(
+          FontAwesomeIcons.twitter,
+          color: Color(0xFF4E98E9),
+          size: Sizes.size20 + 10,
+        ),
+        leadingWidth: 100,
+        leading: IconButton(
+          onPressed: () => onInitialTap(context),
+          icon: const Padding(
+            padding:
+                EdgeInsets.only(right: 40), // Adjust this value to your needs
+            child: FaIcon(
+              FontAwesomeIcons.arrowLeft,
+              color: Color(0xFF101318),
+            ),
+          ),
+        ),
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        elevation: 0,
+      ),
+    );
   }
 }
