1b01527b4f1b1a2874375cdb00ca2a2fcbe2505e
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-25 23:51:06 +0900

    [Password Screen] Green check icon appears for validated password

--- lib/authentication/password_screen.dart
+++ lib/authentication/password_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:twitter_clone/constants/gaps.dart';
 import 'package:twitter_clone/constants/sizes.dart';
+import 'package:twitter_clone/onboarding/interests_screen.dart';
 
 class PasswordScreen extends StatefulWidget {
   const PasswordScreen({super.key});
@@ -14,6 +15,8 @@ class _PasswordScreenState extends State<PasswordScreen> {
   final TextEditingController _passwordController = TextEditingController();
 
   String _password = "";
+  bool _obscureText = true;
+  bool isButtonEnabled = false;
 
   @override
   void initState() {
@@ -32,11 +35,26 @@ class _PasswordScreenState extends State<PasswordScreen> {
   }
 
   bool _isPasswordValid() {
-    return _password.isNotEmpty && _password.length > 8;
+    return _password.isNotEmpty && _password.length >= 8;
   }
 
   void _onScaffoldTap() {
     FocusScope.of(context).unfocus();
+  }
+
+  void _onNextTap() {
+    if (!_isPasswordValid()) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const InterestsScreen(),
+      ),
+    );
+  }
+
+  void _toggleObscureText() {
+    _obscureText = !_obscureText;
+    setState(() {});
   }
 
   @override
@@ -81,8 +99,34 @@ class _PasswordScreenState extends State<PasswordScreen> {
               Gaps.v44,
               TextField(
                 controller: _passwordController,
+                onEditingComplete: _onNextTap,
                 autocorrect: false,
+                obscureText: _obscureText,
                 decoration: InputDecoration(
+                  suffix: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      GestureDetector(
+                        onTap: _toggleObscureText,
+                        child: FaIcon(
+                          _obscureText
+                              ? FontAwesomeIcons.eye
+                              : FontAwesomeIcons.eyeSlash,
+                          color: Colors.grey.shade500,
+                          size: Sizes.size20,
+                        ),
+                      ),
+                      const SizedBox(
+                          width:
+                              10), // Adjust this value to control the spacing between the icons
+                      if (_isPasswordValid())
+                        const FaIcon(
+                          FontAwesomeIcons.circleCheck,
+                          color: Colors.green,
+                          size: Sizes.size20,
+                        ),
+                    ],
+                  ),
                   hintText: "Password",
                   enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(
@@ -94,18 +138,58 @@ class _PasswordScreenState extends State<PasswordScreen> {
                       color: Colors.grey.shade400,
                     ),
                   ),
-                  suffixIcon: Row(
-                    mainAxisSize: MainAxisSize.min, // Add this line
-                    children: [
-                      FaIcon(
-                        FontAwesomeIcons.eyeSlash,
-                        color: Colors.grey.shade500,
-                        size: Sizes.size20,
-                      ),
-                    ],
-                  ),
                 ),
                 cursorColor: Theme.of(context).primaryColor,
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                ),
+                child: GestureDetector(
+                  onTap: isButtonEnabled ? _onNextTap : null,
+                  child: AnimatedContainer(
+                    duration: const Duration(
+                        milliseconds:
+                            300), // animation duration for the container
+                    curve: Curves.easeOut, // easing for the container
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 16,
+                      horizontal: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      color: isButtonEnabled
+                          ? const Color(0xFF101318)
+                          : Colors.grey, // conditional color
+                      borderRadius: BorderRadius.circular(27),
+                      border: Border.all(
+                        color: Colors.grey.shade300,
+                        width: Sizes.size2,
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AnimatedDefaultTextStyle(
+                          style: TextStyle(
+                            color: isButtonEnabled
+                                ? Colors.white
+                                : const Color.fromARGB(255, 229, 226, 226),
+                            fontSize: Sizes.size16 + 3,
+                            fontWeight: FontWeight.w700,
+                          ),
+                          duration: const Duration(
+                              milliseconds:
+                                  300), // animation duration for the text
+                          // easing for the text
+                          child: const Text('Next'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
---
+++ lib/onboarding/interests_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InterestsScreen extends StatefulWidget {
+  const InterestsScreen({super.key});
+
+  @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
