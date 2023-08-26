5cedfc969ac09bddc09337271f50f480629e0cdd
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-26 22:04:36 +0900

    [Interests Screen 2] Rendered Appbar

--- lib/onboarding/interests_screen_two.dart
+++ lib/onboarding/interests_screen_two.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/sizes.dart';
 
 class InterestsScreenTwo extends StatefulWidget {
   const InterestsScreenTwo({super.key});
@@ -8,8 +10,35 @@ class InterestsScreenTwo extends StatefulWidget {
 }
 
 class _InterestsScreenTwoState extends State<InterestsScreenTwo> {
+  void onInitialTap(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).secondaryHeaderColor,
+      appBar: AppBar(
+        title: const FaIcon(
+          FontAwesomeIcons.twitter,
+          color: Color(0xFF4E98E9),
+          size: Sizes.size20 + 10,
+        ),
+        leadingWidth: 100,
+        leading: IconButton(
+          onPressed: () => onInitialTap(context),
+          icon: const Padding(
+            padding:
+                EdgeInsets.only(right: 40), // Adjust this value to your needs
+            child: FaIcon(
+              FontAwesomeIcons.arrowLeft,
+              color: Color(0xFF101318),
+            ),
+          ),
+        ),
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        elevation: 0,
+      ),
+    );
   }
 }
