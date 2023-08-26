bbf84731c42475af970a0bd133b9221393550aeb
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-26 21:49:25 +0900

    [Interests Screen] Enabled button when 3 containers are clicked

--- lib/onboarding/interests_screen.dart
+++ lib/onboarding/interests_screen.dart
@@ -36,6 +36,8 @@ class InterestsScreen extends StatefulWidget {
 }
 
 class _InterestsScreenState extends State<InterestsScreen> {
+  final List<String> _selectedInterests = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,37 +78,126 @@ class _InterestsScreenState extends State<InterestsScreen> {
               ),
               Gaps.v64,
               Wrap(
-                runSpacing: 20,
-                spacing: 20,
+                runSpacing: 10,
+                spacing: 10,
                 children: [
                   for (var interest in interests)
-                    Container(
-                      width: 160,
-                      height: 90,
-                      padding: const EdgeInsets.symmetric(
-                        vertical: Sizes.size24,
-                        horizontal: Sizes.size12,
-                      ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          (Sizes.size32),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.05),
-                            blurRadius: 5,
-                            spreadRadius: 5,
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (_selectedInterests.contains(interest)) {
+                            _selectedInterests.remove(interest);
+                          } else if (_selectedInterests.length < 3) {
+                            _selectedInterests.add(interest);
+                          }
+                        });
+                      },
+                      child: Container(
+                        width: 170,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          color: _selectedInterests.contains(interest)
+                              ? Colors.blue
+                              : Colors.white,
+                          borderRadius: BorderRadius.circular(
+                            (Sizes.size16),
                           ),
-                        ],
-                      ),
-                      child: Text(
-                        interest,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
+                          border: Border.all(
+                            color: Colors.grey.shade300,
+                          ),
+                        ),
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  bottom: Sizes
+                                      .size12, // Adjust this padding to control vertical distance from the bottom
+                                  left: Sizes
+                                      .size12, // Adjust this padding to control horizontal distance from the left
+                                ),
+                                child: Text(
+                                  interest,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: _selectedInterests.contains(interest)
+                                        ? Colors.white
+                                        : Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top:
+                                  8, // Adjust this value for vertical positioning
+                              right:
+                                  8, // Adjust this value for horizontal positioning
+                              child: _selectedInterests.contains(interest)
+                                  ? const FaIcon(
+                                      FontAwesomeIcons
+                                          .checkCircle, // Corrected the icon reference
+                                      color: Colors.white,
+                                      size: 20, // Adjust the size as needed
+                                    )
+                                  : Container(), // Use an empty Container for interests not selected
+                            ),
+                          ],
                         ),
                       ),
                     ),
                 ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 8,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 10,
+            left: 40,
+            right: 40,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '${_selectedInterests.length} of 3 selected',
+                style: TextStyle(
+                  color: Colors.grey.shade400,
+                ),
+              ),
+              GestureDetector(
+                onTap: _selectedInterests.length == 3
+                    ? () {
+                        // Place your "Next" action logic here
+                      }
+                    : null,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: Sizes.size10,
+                    horizontal: Sizes.size20,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: _selectedInterests.length == 3
+                        ? const Color(0xFF101318)
+                        : Colors.grey,
+                  ),
+                  child: Text(
+                    'Next',
+                    style: TextStyle(
+                      color: _selectedInterests.length == 3
+                          ? Colors.white
+                          : const Color.fromARGB(255, 229, 226, 226),
+                      fontSize: Sizes.size16 + 2,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
