229911e373f08349266f006e2a0412f86b55bcd0
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-30 22:15:50 +0900

    [Bottom Sheet - Report] rendered contents within modal

--- lib/home/widgets/bottom_sheet_report.dart
+++ lib/home/widgets/bottom_sheet_report.dart
@@ -7,6 +7,105 @@ class ReportModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final tiles = <Widget>[
+      const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Divider(
+          color: Colors.white,
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 8.0), // Adjust the gap size as needed
+        child: ListTile(
+          title: Text(
+            "I just don't like it",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 8.0), // Adjust the gap size as needed
+        child: ListTile(
+          title: Text(
+            "It's unlawful content under NetzDG",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 8.0), // Adjust the gap size as needed
+        child: ListTile(
+          title: Text(
+            "It's spam",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 8.0), // Adjust the gap size as needed
+        child: ListTile(
+          title: Text(
+            "Hate speech or symbols",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 8.0), // Adjust the gap size as needed
+        child: ListTile(
+          title: Text(
+            "Nudity or sexual activity",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 8.0), // Adjust the gap size as needed
+        child: ListTile(
+          title: Text(
+            "Verbal abuse",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        ),
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 8.0), // Adjust the gap size as needed
+        child: ListTile(
+          title: Text(
+            "Regular crashes",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+            ),
+          ),
+        ),
+      ),
+    ];
+
+    final dividedTiles = ListTile.divideTiles(
+      context: context,
+      tiles: tiles,
+      color: Colors.grey, // Color of the divider, optional
+    ).toList();
+
     final size = MediaQuery.of(context).size;
     return Container(
       height: size.height * 0.73,
@@ -22,54 +121,57 @@ class ReportModal extends StatelessWidget {
           ),
         ),
       ),
-      child: Column(
-        children: [
-          Gaps.v10,
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 8.0),
-            width: 50.0,
-            height: 4.0,
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              borderRadius: BorderRadius.circular(2.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Gaps.v10,
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              width: 50.0,
+              height: 4.0,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(2.0),
+              ),
             ),
-          ),
-          Gaps.v10,
-          const Text(
-            "Report",
-            style: TextStyle(
-              fontSize: Sizes.size20 + Sizes.size1,
-              fontWeight: FontWeight.w600,
+            Gaps.v10,
+            const Text(
+              "Report",
+              style: TextStyle(
+                fontSize: Sizes.size20 + Sizes.size1,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          Gaps.v10,
-          Divider(
-            color: Colors.grey.shade300,
-          ),
-          Gaps.v4,
-          const ListTile(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Why are you reporting this thread?",
-                  style: TextStyle(
-                    fontSize: Sizes.size16 + Sizes.size2,
-                    fontWeight: FontWeight.w600,
+            Gaps.v10,
+            Divider(
+              color: Colors.grey.shade300,
+            ),
+            Gaps.v4,
+            const ListTile(
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Why are you reporting this thread?",
+                    style: TextStyle(
+                      fontSize: Sizes.size16 + Sizes.size2,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                Gaps.v10,
-                Text(
-                  "Your report is anonymous, except if you're reporting an intellectual property infringement. If someone is in immediate danger, call the local emergency services - don't wait.",
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: Sizes.size16 + Sizes.size1,
+                  Gaps.v10,
+                  Text(
+                    "Your report is anonymous, except if you're reporting an intellectual property infringement. If someone is in immediate danger, call the local emergency services - don't wait.",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: Sizes.size16 + Sizes.size1,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            ...dividedTiles,
+          ],
+        ),
       ),
     );
   }
