8196f22c283e3fa2f373d208f1dabf320f15d12d
HyunJun Baek <hjbaek5796@gmail.com>
2023-08-30 20:06:12 +0900

    [Bottom Sheet] Rendered a modal dash for indication

--- lib/home/widgets/bottom_sheet.dart
+++ lib/home/widgets/bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/constants/gaps.dart';
 import 'package:twitter_clone/constants/sizes.dart';
 
 class BottomSheetModal extends StatefulWidget {
@@ -16,9 +17,27 @@ class _BottomSheetModalState extends State<BottomSheetModal> {
       decoration: const BoxDecoration(
         color: Colors.white, // You need to specify a color here.
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(Sizes.size20),
-          topRight: Radius.circular(Sizes.size20),
+          topLeft: Radius.circular(
+            Sizes.size20,
+          ),
+          topRight: Radius.circular(
+            Sizes.size20,
+          ),
         ),
+      ),
+      child: Column(
+        children: [
+          Gaps.v2,
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            width: 50.0,
+            height: 4.0,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(2.0),
+            ),
+          ),
+        ],
       ),
     );
   }
