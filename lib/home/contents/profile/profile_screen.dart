8e4e6c1e71e051cdfb009c9e310558a0f799f69d
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-04 23:37:58 +0900

    [Profile Screen] rendered name and avatar

--- lib/home/contents/profile/profile_screen.dart
+++ lib/home/contents/profile/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/gaps.dart';
 import 'package:twitter_clone/constants/sizes.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -39,6 +40,71 @@ class _ProfileScreenState extends State<ProfileScreen> {
               ),
             ),
           ],
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Jun",
+                        style: TextStyle(
+                          fontSize: Sizes.size28,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            "jun_gamer",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: Sizes.size16 + Sizes.size1,
+                            ),
+                          ),
+                          Gaps.h4,
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: Sizes.size8,
+                              horizontal: Sizes.size8,
+                            ),
+                            decoration: BoxDecoration(
+                              color: const Color(0xFFF5F6F6),
+                              borderRadius: BorderRadius.circular(40),
+                            ),
+                            child: Text(
+                              "threads.net",
+                              style: TextStyle(
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 16.0),
+                  width: 65,
+                  height: 65,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.contain,
+                      image: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/69138182?v=4"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ],
     );
