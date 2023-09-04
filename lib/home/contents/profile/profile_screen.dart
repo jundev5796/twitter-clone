3c0d6f114f2b408e591e557bd4575cc159d8dbfe
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-04 21:25:43 +0900

    [Profile Screen] rendered icons on appbar

--- lib/home/contents/profile/profile_screen.dart
+++ lib/home/contents/profile/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/constants/sizes.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -10,6 +12,35 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          leading: const Icon(
+            Icons.language,
+            color: Colors.black,
+            size: Sizes.size32,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const FaIcon(
+                FontAwesomeIcons.instagram,
+                color: Colors.black,
+                size: Sizes.size32,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.segment,
+                color: Colors.black,
+                size: Sizes.size32,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
