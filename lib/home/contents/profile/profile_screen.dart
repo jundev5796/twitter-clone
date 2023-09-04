a030898561ccfe68c5981a1e9164d8802d887d0b
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-05 00:00:19 +0900

    [Profile Screen] rendered followers count and avatars

--- lib/home/contents/profile/profile_screen.dart
+++ lib/home/contents/profile/profile_screen.dart
@@ -44,63 +44,101 @@ class _ProfileScreenState extends State<ProfileScreen> {
         SliverToBoxAdapter(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Row(
+            child: Column(
               children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Jun",
-                        style: TextStyle(
-                          fontSize: Sizes.size28,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      Row(
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           const Text(
-                            "jun_gamer",
+                            "Jun",
                             style: TextStyle(
-                              color: Colors.black,
-                              fontSize: Sizes.size16 + Sizes.size1,
+                              fontSize: Sizes.size28,
+                              fontWeight: FontWeight.w700,
                             ),
                           ),
-                          Gaps.h4,
-                          Container(
-                            padding: const EdgeInsets.symmetric(
-                              vertical: Sizes.size8,
-                              horizontal: Sizes.size8,
-                            ),
-                            decoration: BoxDecoration(
-                              color: const Color(0xFFF5F6F6),
-                              borderRadius: BorderRadius.circular(40),
-                            ),
-                            child: Text(
-                              "threads.net",
-                              style: TextStyle(
-                                color: Colors.grey.shade500,
+                          Row(
+                            children: [
+                              const Text(
+                                "jun_gamer",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: Sizes.size16 + Sizes.size1,
+                                ),
                               ),
-                            ),
+                              Gaps.h4,
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: Sizes.size8,
+                                  horizontal: Sizes.size8,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFF5F6F6),
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                                child: Text(
+                                  "threads.net",
+                                  style: TextStyle(
+                                    color: Colors.grey.shade500,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 16.0),
-                  width: 65,
-                  height: 65,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.contain,
-                      image: NetworkImage(
-                          "https://avatars.githubusercontent.com/u/69138182?v=4"),
                     ),
-                  ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 16.0),
+                      width: 65,
+                      height: 65,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.contain,
+                          image: NetworkImage(
+                              "https://avatars.githubusercontent.com/u/69138182?v=4"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v14,
+                const Row(
+                  children: [
+                    Text(
+                      "Game enthusiast!",
+                      style: TextStyle(
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v24,
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 12,
+                      foregroundImage: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/1?v=4"),
+                    ),
+                    const CircleAvatar(
+                      radius: 12,
+                      foregroundImage: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/3612017?v=4"),
+                    ),
+                    Gaps.h14,
+                    Text(
+                      "2 followers",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size18,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
