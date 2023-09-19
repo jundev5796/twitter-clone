9dd6fee92cd0df1fad966b147bfbd8ac7148f5ca
HyunJun Baek <hjbaek5796@gmail.com>
2023-09-19 23:54:48 +0900

    storing newly created accounts on firebase

--- lib/home/contents/users/models/user_profile_model.dart
+++ lib/home/contents/users/models/user_profile_model.dart
@@ -1,12 +1,14 @@
 class UserProfileModel {
   final String username;
   final String nickname;
+  final String bio;
   final String uid;
   // final bool hasAvatar;
 
   UserProfileModel({
     required this.username,
     required this.nickname,
+    required this.bio,
     required this.uid,
     // required this.hasAvatar,
   });
@@ -14,5 +16,15 @@ class UserProfileModel {
   UserProfileModel.empty()
       : username = "",
         nickname = "",
+        bio = "",
         uid = "";
+
+  Map<String, String> toJson() {
+    return {
+      "username": username,
+      "nickname": nickname,
+      "bio": bio,
+      "uid": uid,
+    };
+  }
 }
--- lib/home/contents/users/view_models/users_view_model.dart
+++ lib/home/contents/users/view_models/users_view_model.dart
@@ -3,27 +3,33 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/home/contents/users/models/user_profile_model.dart';
+import 'package:twitter_clone/home/contents/users/repos/user_repo.dart';
 
 class UsersViewModel extends AsyncNotifier<UserProfileModel> {
+  late final UserRepository _repository;
+
   @override
   FutureOr<UserProfileModel> build() {
+    _repository = ref.read(userRepo);
     return UserProfileModel.empty();
   }
 
-  Future<void> createAccount(UserCredential credential) async {
+  Future<void> createProfile(UserCredential credential) async {
     if (credential.user == null) {
       throw Exception("Account not created");
     }
-    state = AsyncValue.data(
-      UserProfileModel(
-        username: "Anonymous",
-        nickname: "Anonymous",
-        uid: credential.user!.uid,
-      ),
+    state = const AsyncValue.loading();
+    final profile = UserProfileModel(
+      username: "Anonymous",
+      nickname: "Anonymous",
+      bio: "Undefined",
+      uid: credential.user!.uid,
     );
+    await _repository.createProfile(profile);
+    state = AsyncValue.data(profile);
   }
 }
 
-final usersProvider = AsyncNotifierProvider(
+final usersProvider = AsyncNotifierProvider<UsersViewModel, UserProfileModel>(
   () => UsersViewModel(),
 );
